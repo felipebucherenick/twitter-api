15438f87625565c8c34ccce32792d6ab3e356126
Felipe Bucherenick <felipebucherenick@gmail.com>
2021-11-11 18:25:56 -0300

    post_a_tweet

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ class Tweet(BaseModel):
         max_length=256
     )
     created_at: datetime = Field(default=datetime.now())
-    update_at: Optional[datetime] = Field(default=None)
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
 
@@ -203,6 +203,7 @@ def update_a_user():
 def home():
     return {"Twitter API": "Working!"}
 
+
 # Post a tweet
 
 
@@ -213,8 +214,32 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    # Post a tweet  
+    ## This path operation post a tweet in the app.  
+    ### Parameters:  
+      - #### Request Body:  
+          - **tweet:** *Tweet*  
+    ### Returns a JSON with the basic tweet information:  
+      - **tweet_id:** *UUID*  
+      - **content:** *str*  
+      - **created_at:** *datetime*  
+      - **updated_at:** *Optional[datetime]*  
+      - **by:** *User*            
+    """
+    with open('tweets.json', 'r+', encoding='utf-8') as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict['tweet_id'] = str(tweet_dict["tweet_id"])
+        tweet_dict['created_at'] = str(tweet_dict["created_at"])
+        tweet_dict['updated_at'] = str(tweet_dict["updated_at"])
+        tweet_dict['by']['user_id'] = str(tweet_dict["by"]['user_id'])
+        tweet_dict['by']['birth_date'] = str(tweet_dict["by"]['birth_date'])
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 # Show a tweet
 
